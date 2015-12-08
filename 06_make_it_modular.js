580fed0622612e300b06e1194d957646de11fbd0
Brittany See <brittanyjsee@gmail.com>
2015-12-08 00:48:08 +0000

    exercises 3-6

---
+++ 06_make_it_modular.js
@@ -0,0 +1,6 @@
+require('./modular.lib.js')(process.argv[2], process.argv[3],
+  function(e, valid_files){
+    if(e) console.log(e)
+    else valid_files.forEach(function(file){ console.log(file) })
+  }
+)
